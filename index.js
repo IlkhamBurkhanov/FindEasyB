87cc2fa3696a1bbccc2529991ff20bc07d0b8cbf
IlkhamBurkhonov <ilkhomjonburkhonov207@gmail.com>
2025-02-07 11:57:46 +0500

    fix: dotenv

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config(); // .env ni yuklash
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
 const express = require('express');
 const mongoose = require('mongoose');
